64d01c56e86df1b3063cb6b7c531de8e3d573b06
saulp <saulp_zhang@foxmail.com>
2026-03-28 18:08:55 +0800

    record

--- record_wandb.py
+++ record_wandb.py
@@ -9,6 +9,7 @@ def get_wandb_run():
     global run
     if run is None:
         run = wandb.init(
+            id = "funsearch_ori",
             # Set the wandb entity where your project will be logged (generally your team name).
             entity="qijizhang5-city-university-of-hong-kong",
             # Set the wandb project where this run will be logged.
